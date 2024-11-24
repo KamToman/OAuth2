770f543e9345f2cf20f042f92ce1312ac847dc47
KamToman <152777238+KamToman@users.noreply.github.com>
2024-11-24 15:58:48 +0100

    Update app.py

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ CLIENT_ID = os.getenv("CLIENT_ID")
 CLIENT_SECRET = os.getenv("CLIENT_SECRET")
 TENANT_ID = os.getenv("TENANT_ID")
 REDIRECT_URI = os.getenv("REDIRECT_URI")
-AUTHORITY = f"https://login.microsoftonline.com/{TENANT_ID}"
+AUTHORITY = f"https://login.microsoftonline.com/92490e38-49cf-409e-97cc-9de5194809ba"
 SCOPES = ["User.Read", "api://{CLIENT_ID}/access_as_user"]
 
 # Konfiguracja MSAL
